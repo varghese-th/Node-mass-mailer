7cd7eb57feffa02d7affaaafadc12348e4d02a5b
toobler <toobler@tblr.dcm4che.org>
2018-08-30 18:27:28 +0530

    minor fix

--- controller/mail-sender.js
+++ controller/mail-sender.js
@@ -39,37 +39,179 @@ module.exports = {
 	//     next();
 	// }
 
+	// sendingMail: function (req, res, next) {
+	   
+	//     let transporter = nodemailer.createTransport(config.authenticationMail);
+
+	// 	let successMailArray = [];
+
+	//     for (var mailToListLoop=0; mailToListLoop<=(mailToList.length-1); mailToListLoop++) {
+
+	// 	    // setup email data with unicode symbols
+	// 	    let mailOptions = {
+	// 	        from: '"' + config.mail.mainname + '" <'+ config.mail.mainmail + '>', // sender address
+	// 	        to: mailToList[mailToListLoop], // list of receivers
+	// 	        subject: mailSubject,
+	// 	        // text: mailText
+	// 	        html: '<strong>Name:</strong> ' + mailToListName[mailToListLoop]
+	// 	    };
+
+	// 	    // send mail with defined transport object
+	// 	    transporter.sendMail(mailOptions, (error, info) => {
+	// 	        if (error) {
+	// 	            return console.log(error);
+	// 	        }
+	// 	        else {
+	// 	        	successMailArray.push(mailOptions.to);
+	// 	        	console.log('Mail sent to (' + successMailArray.length + '/' + mailToList.length + ') contacts(s)');
+	// 	        }
+	// 	    });
+
+	// 	}
+
+	// 	next();
+	// }
+
+
+	// sendingMail: function (req, res, next) {
+	   
+	//     let transporter = nodemailer.createTransport(config.authenticationMail);
+
+	// 	let successMailArray = [];
+
+	// 	function toMailFunction() 
+	// 	{
+	// 		return new Promise(function (resolve, reject)	{    
+	// 			for (var mailToListLoop=0; mailToListLoop<=(mailToList.length-1); mailToListLoop++) 
+	// 			{
+
+	// 			    // setup email data with unicode symbols
+	// 			    let mailOptions = {
+	// 			        from: '"' + config.mail.mainname + '" <'+ config.mail.mainmail + '>', // sender address
+	// 			        to: mailToList[mailToListLoop], // list of receivers
+	// 			        subject: mailSubject,
+	// 			        // text: mailText
+	// 			        html: '<strong>Name:</strong> ' + mailToListName[mailToListLoop]
+	// 			    };
+
+	// 			    // send mail with defined transport object
+	// 			    transporter.sendMail(mailOptions, (error, info) => {
+	// 			        // console.log(mailOptions.to);
+	// 			        if (error) {
+	// 			            return console.log(error);
+	// 			        }
+	// 			        else {
+	// 			        	successMailArray.push(mailOptions.to);
+	// 			        	console.log('Mail sent to (' + successMailArray.length + '/' + mailToList.length + ') contacts(s)');
+	// 			        }
+	// 			    });
+
+	// 			}
+	// 			resolve(successMailArray);
+	// 		});
+	// 	}
+
+	// 	async function toConsoleFunction() {
+	// 		let successMailArrayy = await toMailFunction();
+	// 		console.log('Success: ' + successMailArrayy.length);
+	// 	}
+
+	// 	toConsoleFunction();
+
+	// 	next();
+	// }
+
+
+	// sendingMail: function (req, res, next) {
+	   
+	//     let transporter = nodemailer.createTransport(config.authenticationMail);
+
+	// 	let successMailArray = [];
+
+	// 	function toMailFunction() 
+	// 	{
+	// 		return new Promise( resolve =>	{    
+
+	// 			    // setup email data with unicode symbols
+	// 			    let mailOptions = {
+	// 			        from: '"' + config.mail.mainname + '" <'+ config.mail.mainmail + '>', // sender address
+	// 			        to: 'varghese.tblr@gmail.com', // list of receivers
+	// 			        subject: mailSubject,
+	// 			        // text: mailText
+	// 			        html: '<strong>Name:</strong> '
+	// 			    };
+
+	// 			    // send mail with defined transport object
+	// 			    transporter.sendMail(mailOptions, (error) => {
+	// 			        // console.log(mailOptions.to);
+	// 			        if (error) {
+	// 			            return console.log(error);
+	// 			        }
+	// 			        else {
+	// 			        	successMailArray.push(mailOptions.to);
+	// 			        	console.log('Mail sent to (' + successMailArray.length + '/' + mailToList.length + ') contacts(s)');
+	// 			        }
+	// 			    });
+
+
+	// 			resolve(successMailArray);
+	// 		});
+	// 	}
+
+	// 	async function toConsoleFunction() {
+	// 		let successMailArray = await toMailFunction();
+	// 		console.log('Success: ' + successMailArray.length);
+	// 	}
+
+	// 	toConsoleFunction();
+
+	// 	next();
+	// }
+
 	sendingMail: function (req, res, next) {
 	   
 	    let transporter = nodemailer.createTransport(config.authenticationMail);
 
 		let successMailArray = [];
 
-	    for (var mailToListLoop=0; mailToListLoop<=(mailToList.length-1); mailToListLoop++) {
+		function toMailFunction(callback) 
+		{
+ 
 
-		    // setup email data with unicode symbols
-		    let mailOptions = {
-		        from: '"' + config.mail.mainname + '" <'+ config.mail.mainmail + '>', // sender address
-		        to: mailToList[mailToListLoop], // list of receivers
-		        subject: mailSubject,
-		        // text: mailText
-		        html: '<strong>Name:</strong> ' + mailToListName[mailToListLoop]
-		    };
+				    // setup email data with unicode symbols
+				    let mailOptions = {
+				        from: '"' + config.mail.mainname + '" <'+ config.mail.mainmail + '>', // sender address
+				        to: 'varghese.tblr@gmail.com', // list of receivers
+				        subject: mailSubject,
+				        // text: mailText
+				        html: '<strong>Name:</strong> '
+				    };
 
-		    // send mail with defined transport object
-		    transporter.sendMail(mailOptions, (error, info) => {
-		        if (error) {
-		            return console.log(error);
-		        }
-		        else {
-		        	successMailArray.push(mailOptions.to);
-		        	console.log('Mail sent to (' + successMailArray.length + '/' + mailToList.length + ') contacts(s)');
-		        }
-		    });
+				    // send mail with defined transport object
+				    transporter.sendMail(mailOptions, (error) => {
+				        // console.log(mailOptions.to);
+				        if (error) {
+				            return console.log(error);
+				        }
+				        else {
+				        	successMailArray.push(mailOptions.to);
+				        	console.log('Mail sent to (' + successMailArray.length + '/' + mailToList.length + ') contacts(s)');
+				        }
+				    });
+
+
+				callback(successMailArray);
 
 		}
+
+		toMailFunction() {
+			let successMailArray = await toMailFunction();
+			console.log('Success: ' + successMailArray.length);
+		}
+
 
 		next();
 	}
 
 }
+// https://stackoverflow.com/questions/47158979/node-wait-for-async-function-before-continue
